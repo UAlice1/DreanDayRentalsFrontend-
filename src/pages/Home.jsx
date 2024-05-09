162114541413577298329f39aa1f4c18d1331fd4
Brice Kelly <ntirukelly@gmail.com>
2024-05-09 12:32:24 +0200

    new-commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,17 @@ function Home() {
 <div className="video-background bg-black flex justify-center items-center">
   <video autoPlay muted loop id="video-background" className="absolute inset-0 w-full h-full object-cover">
     <source src="/video.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-  <div className="content text-center">
-    <h2 className='wlc text-3xl text-white'>
+    <h2 className='wlc text-3xl text-black'>
       Welcome to Dream's Day Rentals
     </h2>
-  </div>
+   
+  </video>
+  <div className="flex justify-center items-center z-40 left-0 w-full h-10vh absolute p-10">
+  <h2 className="wlc text-4xl text-white">
+    Welcome to wedding planner
+  </h2>
+  <p className="text-4xl text-white mb-4">Dream's Days Rentals</p>
+</div>
 </div>
   )
 }

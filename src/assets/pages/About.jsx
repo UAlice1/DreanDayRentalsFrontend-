c437946e5a08fbfded4fec5c08228a3382b41539
Brice Kelly <ntirukelly@gmail.com>
2024-05-23 09:34:49 +0200

    new-changes

--- src/assets/pages/About.jsx
+++ src/assets/pages/About.jsx
@@ -2,24 +2,11 @@ import React from 'react'
 
 function About() {
   return (
-<div className='flex items-center justify-center p-36 mt-32'>
-  <div className='p-8 md:p-12 flex flex-col md:flex-row md:items-center bg-white rounded-xl shadow-lg'>
-    <div className="md:mr-8">
-      <img className='rounded-lg md:rounded-xl' src="./images/wed.png" alt="Wedding Team" />
-    </div>
-    <div className='md:ml-8'>
-      <h2 className='italic text-3xl font-bold mb-4'>About Us</h2>
-      <p className='italic mb-4'>Hello! We're the Wedding Team</p>
-      <p className="text-base text-gray-700 leading-normal font-serif">
-      We are a group of wedding planners who love making your special day perfect.
-  Whether you want a small, intimate wedding or a big celebration, we take care of everything.
-  Our services include finding the right venue, <br />designing the theme, coordinating with vendors, and managing the day itself.
-  We’re here to make your <br />wedding beautiful and stress-free, reflecting your unique love story.
-  Let us help you create unforgettable memories.
-      </p>
-    </div>
-  </div>
+<>
+<div className='container text-center p-8'>
+  <h1>Welcome to wedding planner</h1>
 </div>
+</>
   )
 }
 
--- src/assets/pages/Bookings.jsx
+++ src/assets/pages/Bookings.jsx
@@ -1,52 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaLocationDot } from "react-icons/fa6";
+import { GoPeople } from "react-icons/go";
+import { SiSanfranciscomunicipalrailway } from "react-icons/si";
 
-function Bookings() {
+const BookingPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    eventDate: '',
+    eventLocation: '',
+    additionalNotes: ''
+  });
+
+  const [showSecondImage, setShowSecondImage] = useState(false);
+
+  const isFormValid = () => {
+    return (
+      formData.firstName !== '' &&
+      formData.lastName !== '' &&
+      formData.email !== '' &&
+      formData.phoneNumber !== '' &&
+      formData.eventDate !== ''
+    );
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormValid()) {
+      console.log(formData);
+    } else {
+      alert('Please fill in all required fields.');
+    }
+  };
+
+  const handleToggleImage = () => {
+    setShowSecondImage(!showSecondImage);
+  };
+
   return (
-<div className="bookings p-6">
-  <h2 className="text-3xl text-center mb-16">Bookings Halls</h2>
-  <div className="mt-11 flex flex-col md:flex-row flex-wrap justify-center gap-8">
-    <div className="card bg-white rounded-lg shadow-lg p-6 max-w-sm">
-      <img className="w-full h-56 object-cover rounded-t-lg" src="/public/images/des4.jpg" alt="Booked hall" />
-      <div className="mt-4">
-        <span className="block text-gray-700">Location: Kigali</span>
-        <span className="block text-gray-700">Price: frw</span>
-        <span className="block text-gray-700">Max: 200 people</span>
-        <a href="#" className="block mt-4 bg-[#a6c1ee] text-white py-2 px-4 rounded hover:bg-blue-600 text-center">Booked</a>
+    <div className="flex flex-col md:flex-row justify-center p-4">
+      <div className="md:w-3/5 p-4 border border-gray-300 rounded-xl mt-10 md:mr-5">
+        <img src={showSecondImage ? "/images/des6.jpg" : "/images/des9.jpg" } alt="Hall" className="h-64 md:h-80 w-full rounded-xl object-cover"/>
+        <div className="mt-4">
+          <h2 className="text-lg font-semibold">Luxurious Event Hall</h2>
+          <h1 className="flex items-center mt-2"><FaLocationDot className="mr-2" /> Kigalo city</h1>
+          <div className="flex flex-col md:flex-row justify-between mt-2">
+            <p className="text-sm text-gray-600 flex items-center mb-2 md:mb-0"><FaLocationDot className="mr-2" /> Convetion centre, Central Plaza</p>
+            <p className="text-sm text-gray-600 flex items-center mb-2 md:mb-0"><GoPeople className="mr-2" /> 200 per/people</p>
+            <p className="text-sm text-gray-600 flex items-center"><SiSanfranciscomunicipalrailway className="mr-2" /> Renting Cost: $5000/day</p>
+          </div>
+          <button onClick={handleToggleImage} className="inline-block bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600 mt-4">More About</button>
+          <h1 className="mt-4">Description</h1>
+          <p className="mt-2 text-sm text-gray-700">This luxurious event hall situated in the heart of the city offers a sophisticated venue for various occasions. With its modern amenities and spacious layout, it can accommodate up to 200 guests comfortably. Perfect for weddings, corporate events, and other special occasions.</p>
+        </div>
       </div>
-    </div>
-    <div className="card bg-white rounded-lg shadow-lg p-6 max-w-sm">
-      <img className="w-full h-56 object-cover rounded-t-lg" src="/public/images/des4.jpg" alt="Booked hall" />
-      <div className="mt-4">
-        <span className="block text-gray-700">Location: Kigali</span>
-        <span className="block text-gray-700">Price: frw</span>
-        <span className="block text-gray-700">Max: 200 people</span>
-        <a href="#" className="block mt-4 bg-[#a6c1ee] text-white py-2 px-4 rounded hover:bg-blue-600 text-center">Booked</a>
-      </div>
-    </div>
-    <div className="card bg-white rounded-lg shadow-lg p-6 max-w-sm">
-      <img className="w-full h-56 object-cover rounded-t-lg" src="/public/images/des4.jpg" alt="Booked hall" />
-      <div className="mt-4">
-        <span className="block text-gray-700">Location: Kigali</span>
-        <span className="block text-gray-700">Price: frw</span>
-        <span className="block text-gray-700">Max: 200 people</span>
-        <a href="#" className="block mt-4 bg-[#a6c1ee] text-white py-2 px-4 rounded hover:bg-blue-600 text-center">Booked</a>
-      </div>
-    </div>
-    <div className="card bg-white rounded-lg shadow-lg p-6 max-w-sm">
-      <img className="w-full h-56 object-cover rounded-t-lg" src="/public/images/des4.jpg" alt="Booked hall" />
-      <div className="mt-4">
-        <span className="block text-gray-700">Location: Kigali</span>
-        <span className="block text-gray-700">Price: frw</span>
-        <span className="block text-gray-700">Max: 200 people</span>
-        <a href="#" className="block mt-4 bg-[#a6c1ee] text-white py-2 px-4 rounded hover:bg-blue-600 text-center">Booked</a>
-      </div>
-    </div>
-  </div>
-</div>
-  )
-}
 
-export default Bookings
+      <div className="md:w-2/5 p-4 mt-10 md:ml-5 border border-gray-300 rounded-xl">
+        <h2 className="text-2xl font-semibold text-center">Information</h2>
+        <form onSubmit={handleSubmit} className="mt-8">
+          <div className="mb-4">
+            <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name</label>
+            <input type="text" id="firstName" name="firstName" required value={formData.firstName} onChange={handleInputChange} className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block shadow-sm sm:text-sm border-gray-300 w-full" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">Last Name</label>
+            <input type="text" id="lastName" name="lastName" required value={formData.lastName} onChange={handleInputChange} className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block shadow-sm sm:text-sm border-gray-300 w-full" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+            <input type="email" id="email" name="email" required value={formData.email} onChange={handleInputChange} className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block shadow-sm sm:text-sm border-gray-300 w-full" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-700">Tel</label>
+            <input type="tel" id="phoneNumber" name="phoneNumber" required value={formData.phoneNumber} onChange={handleInputChange} className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block shadow-sm sm:text-sm border-gray-300 w-full" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="eventDate" className="block text-sm font-medium text-gray-700">Event Date</label>
+            <input type="date" id="eventDate" name="eventDate" required value={formData.eventDate} onChange={handleInputChange} className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block shadow-sm sm:text-sm border-gray-300 w-full" />
+          </div>
+          <div className="mb-4 text-center">
+            <button type="submit" className="inline-block bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600">Book Now</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default BookingPage;
 
 
 

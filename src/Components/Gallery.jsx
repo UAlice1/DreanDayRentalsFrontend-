4186814fcebddb32850638864914b8b79580493b
Brice Kelly <ntirukelly@gmail.com>
2024-05-24 10:01:54 +0530

    new-commit

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -3,25 +3,24 @@ import React from 'react'
 function Gallery() {
   return (
     <div>
-        <h2 italic mb-4>See Our Gallery</h2>
-      <div className='flex justify-center items-center h-screen'>
-       <div className='Gallery'>
-        <img src="wed.png" alt="img-Gallery"/>
-       </div>
-       <div className='Gallery'>
-        <img src="wed.png" alt="img-Gallery"/>
-       </div>
-       <div className='Gallery'>
-        <img src="wed.png" alt="img-Gallery"/>
-       </div>
-       <div className='Gallery'>
-        <img src="wed.png" alt="img-Gallery"/>
-       </div>
-       <div className='Gallery'>
-        <img src="wed.png" alt="img-Gallery"/>
-       </div>
+  <div>
+    <h2 className='text-3xl mt-10 font-sans italic text-center'>See Our Gallery</h2>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
+      <div className='Gallery'>
+        <img className='w-full h-auto rounded-lg' src="public/images/des9.jpg" alt="img-Gallery" />
+      </div>
+      <div className='Gallery'>
+        <img className='w-full h-auto rounded-lg' src="public/images/des9.jpg" alt="img-Gallery" />
+      </div>
+      <div className='Gallery'>
+        <img className='w-full h-auto rounded-lg' src="public/images/des9.jpg" alt="img-Gallery" />
+      </div>
+      <div className='Gallery'>
+        <img className='w-full h-auto rounded-lg' src="public/images/des9.jpg" alt="img-Gallery" />
       </div>
     </div>
+  </div>
+</div>
   )
 }
 
